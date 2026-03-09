757f817874897b9e20ad2fff9f0f36af350580f1
Victor Souza <vicentesouzavictor29@gmail.com>
2026-03-09 16:28:52 -0300

    Organizei o código para deixar de forma mais profissional, com comentários, espaçamentos e realocação e organização de comentários

--- Revisão/1-revisão-c.cpp
+++ Revisão/1-revisão-c.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-// REVISÃO 1 - Fácil
+// REVISÃO  - Fácil
 // declare uma variável int x = 10
 
 // crie um ponteiro para ela
@@ -17,37 +17,32 @@
 
 // valor apontado pelo ponteiro
 
-
-// REVISÃO 2 - Fácil
-// Faça um programa que altere o valor de uma variável usando ponteiro.
-
-void ponteiro() {
+void ponteiro () {
     int x = 10;
     int *ptr = &x;
-
-        // Revisão 1
 
         printf("%d \n", x);
         printf("%p\n", &x);
 
-        printf("%d\n", *ptr);
-        printf("%p\n", &ptr);
+        printf("%d\n", *ptr); // exibi o valor do ponteiro
+        printf("%p\n", &ptr); // exibi o endereço do ponteiro
 }
 
-void alternando() {
+// REVISÃO  - Fácil
+// Faça um programa que altere o valor de uma variável usando ponteiro.
+
+void alternando () {
     int y = 5;
     int *ptr = &y;
-
-        // Revisão 2
 
         printf("==================== \n");
         printf("Valor antes: %d\n", y);
 
-        *ptr = 18;
+        *ptr = 18; // assim que se atribui outro valor ao ponteiro ptr
           printf("Valor depois: %d\n", *ptr);
 }
 
-int main() {
+int main () {
     ponteiro();
     alternando();
 
